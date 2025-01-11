c62ad7051cab5b2f817dcb9a3e3213b89399cdbb
Sahil Alamgir <sahil.alamgir@gmail.com>
2025-01-11 15:30:53 -0500

    Create database JS file to establish connection with Postgres database

---
+++ server/db.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    password: process.env.POSTGRES_PASSWORD,
+    host : "localhost",
+    port: 5432,
+    database: "recipe"
+});
+
+module.exports = pool;
